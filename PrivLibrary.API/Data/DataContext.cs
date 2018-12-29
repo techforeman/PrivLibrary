ff3117b6bea6cf225cc1234f52393518cd78a54c
techforeman <majsterjunior@gmail.com>
2018-12-29 08:02:52 +0100

    Initial commit wq

---
+++ PrivLibrary.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+
+
+
+using Microsoft.EntityFrameworkCore;
+using PrivLibrary.API.Models;
+
+namespace PrivLibrary.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values{get; set;}        
+    }
+}
